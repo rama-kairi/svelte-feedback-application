785e7241bf490382b71511c57c4728cedb76e406
Ramananda Kairi <ramananda.kairi@gmail.com>
2022-06-29 22:02:39 +0530

    Initial Commit

---
+++ src/stores.ts
@@ -0,0 +1,25 @@
+import { Writable, writable } from 'svelte/store';
+
+type FeedbackStoreType = {
+  id: number;
+  rating: number;
+  text: string;
+};
+
+export const FeedbackStore: Writable<FeedbackStoreType[]> = writable([
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+]);
